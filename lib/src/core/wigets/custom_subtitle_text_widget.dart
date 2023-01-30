265213dc74890787ab2676c728b89f2b4aa908cd
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-01-30 15:34:27 -0300

    FEAT: componente para subtitulos;

---
+++ lib/src/core/wigets/custom_subtitle_text_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomSubitleTextWidget extends StatelessWidget {
+  final String subtitle;
+  const CustomSubitleTextWidget({Key? key, required this.subtitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 0.01 * size.width),
+      child: Text(
+        subtitle,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 0.04 * size.width,
+          fontWeight: FontWeight.w400,
+          color: Colors.purple.withOpacity(0.6),
+        ),
+      ),
+    );
+  }
+}
