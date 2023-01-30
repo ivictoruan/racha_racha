490937f29ff6fcf2cd4fe7b69065bc8f23426d06
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-01-30 15:33:06 -0300

    FEAT: divisor customizado;

---
+++ lib/src/core/wigets/custom_small_divider_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomSmallDividerWidget extends StatelessWidget {
+  const CustomSmallDividerWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => const Divider(
+        height: 0,
+        color: Colors.purple,
+      );
+}
