e34ceb7130dcade54f9e896b18222c20e9da3fb8
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-01-30 15:16:35 -0300

    FEAT: add appwidget;

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'core/utils/routes/routes.dart';
+
+class RachaRachaApp extends StatelessWidget {
+  const RachaRachaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerDelegate: routes.routerDelegate,
+      routeInformationParser: routes.routeInformationParser,
+      routeInformationProvider: routes.routeInformationProvider,
+      debugShowCheckedModeBanner: false,
+      title: "Racha Racha",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        // useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+}
