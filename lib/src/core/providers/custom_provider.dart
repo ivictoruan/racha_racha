0b0c9d972c8168946f7de9d394afa5496c4d8dea
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-08-01 16:18:41 -0300

    REFACTOR: adicionando novos providers para consumo no app;

--- lib/src/core/providers/custom_provider.dart
+++ lib/src/core/providers/custom_provider.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:racha_racha/src/controllers/is_someone_drinking_controller.dart';
 
 import '../../app_widget.dart';
+import '../../controllers/result_controller.dart';
+import '../../controllers/total_people_controller.dart';
+import '../../controllers/total_value_controller.dart';
 import '../controller/check_controller.dart';
 import '../models/check_model.dart';
 
+/// Classe `CustomProvider` que atua como o widget raiz da aplicação.
+/// Fornece vários controladores utilizando `ChangeNotifierProvider`
+/// para o gerenciamento de estado.
 class CustomProvider extends StatelessWidget {
   const CustomProvider({Key? key}) : super(key: key);
 
@@ -12,12 +19,24 @@ class CustomProvider extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider(
+        ChangeNotifierProvider(
           create: (context) => CheckModel(),
         ),
         ChangeNotifierProvider(
-          create: (context) => CheckController(model: CheckModel()),
-        ),
+            create: (context) =>
+                TotalValueController(model: context.read<CheckModel>())),
+        ChangeNotifierProvider(
+            create: (context) =>
+                TotalPeopleController(model: context.read<CheckModel>())),
+        ChangeNotifierProvider(
+            create: (context) =>
+                IsSomeoneDrinkingController(model: context.read<CheckModel>())),
+        ChangeNotifierProvider(
+            create: (context) =>
+                ResultController(model: context.read<CheckModel>())),
+        ChangeNotifierProvider(
+            create: (context) =>
+                CheckController(model: context.read<CheckModel>())),
       ],
       child: const RachaRachaApp(),
     );
