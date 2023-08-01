8a6dc9298fd281c9f6dbde33faaf171dfc30162b
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-08-01 16:13:05 -0300

    REFACTOR: utilizando classe de controller própria + redução de elevação;

--- lib/src/screens/home/view/widgets/custom_slider.dart
+++ lib/src/screens/home/view/widgets/custom_slider.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../../../core/controller/check_controller.dart';
+import '../../../../controllers/total_people_controller.dart';
 
 class CustomSlider extends StatelessWidget {
   const CustomSlider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<CheckController>(
+    return Consumer<TotalPeopleController>(
       builder: (context, controller, child) {
         Size size = MediaQuery.sizeOf(context);
         return Material(
-          elevation: 10,
+          elevation: 2,
           shadowColor: Colors.deepPurple.withOpacity(0.15),
           borderRadius: BorderRadius.circular(20),
           child: Row(
             children: [
-              // const Icon(Icons.attach_money, ),
               Expanded(
                 child: Slider(
                   value: controller.model.waiterPercentage.toDouble(),
