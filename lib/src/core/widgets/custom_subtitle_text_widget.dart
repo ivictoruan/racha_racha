12ef34e2f5c68a90546e4169a250e85f28013183
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-08-01 16:32:03 -0300

    STYLE: corrigindo nome do arquivo;

--- lib/src/core/widgets/custom_subtitle_text_widget.dart
+++ lib/src/core/widgets/custom_subtitle_text_widget.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
-class CustomSubitleTextWidget extends StatelessWidget {
+class CustomSubtitleTextWidget extends StatelessWidget {
   final String subtitle;
-  const CustomSubitleTextWidget({Key? key, required this.subtitle})
+  const CustomSubtitleTextWidget({Key? key, required this.subtitle})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 0.01 * size.width),
+      padding: EdgeInsets.symmetric(
+        horizontal: 0.05 * size.width,
+        vertical: 0.01 * size.width,
+      ),
       child: Text(
         subtitle,
         textAlign: TextAlign.center,
         style: TextStyle(
           fontSize: 0.04 * size.width,
-          fontWeight: FontWeight.w400,
           color: Colors.deepPurple.withOpacity(0.6),
         ),
       ),
