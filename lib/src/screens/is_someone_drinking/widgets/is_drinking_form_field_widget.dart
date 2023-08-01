8b18f9284089dd522b64d66293fd72afb06ed232
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-08-01 16:08:01 -0300

    REFACTOR: utilizando classe própria;

--- lib/src/screens/is_someone_drinking/widgets/is_drinking_form_field_widget.dart
+++ lib/src/screens/is_someone_drinking/widgets/is_drinking_form_field_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:racha_racha/src/core/controller/check_controller.dart';
+import 'package:racha_racha/src/core/widgets/custom_subtitle_text_widget.dart';
+import '../../../controllers/is_someone_drinking_controller.dart';
+import '../../../core/widgets/custom_title_text_widget.dart';
 import '../fields/total_drink_value_field_widget.dart';
 import '../fields/total_people_drinking_field_widget.dart';
 import 'info_text_alert_widget.dart';
@@ -13,14 +15,39 @@ class IsDrinkingFormFieldWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return SizedBox(
-      child: Consumer<CheckController>(
+      child: Consumer<IsSomeoneDrinkingController>(
         builder: (context, controller, child) {
           return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              const SizedBox(height: 5),
               TotalDrinkValueFieldWidget(controller: controller),
+              const CustomSubtitleTextWidget(
+                subtitle:
+                    "Digite os valores somados das bebidas consumidas (sem taxa de serviço).",
+              ),
+
               SizedBox(height: size.height * 0.02),
+
+              const SizedBox(height: 5),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.groups_3_sharp,
+                    size: 30,
+                    color: Colors.deepPurple,
+                  ),
+                  CustomTitleTextWidget(titleText: "Quantas pessoas beberam?"),
+                ],
+              ),
+              const SizedBox(height: 15),
+
               TotalPeopleDrinkingFieldWidget(controller: controller),
-              if (controller.msgError != "") ...{
+              const CustomSubtitleTextWidget(
+                  subtitle:
+                      "Digite a quantidade de pessoas que consumiram bebidas"),
+              if (controller.msgError.isNotEmpty) ...{
                 SizedBox(height: size.height * 0.015),
                 const InfoTextAlertWidget(),
               },
