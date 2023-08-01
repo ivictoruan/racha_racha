69e7c505a85bcad6f1a7c64321086df2ea6dd029
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2023-08-01 16:14:47 -0300

    REFACTOR: melhorando textos de erros;

--- lib/src/core/utils/errors/check_controller_error_mesages.dart
+++ lib/src/core/utils/errors/check_controller_error_mesages.dart
@@ -12,15 +12,15 @@ class CheckControllerErrorMessages {
 
   /// Mensagem de erro para haver mais pessoas bebendo do que rachando a conta.
   static const String errorMsgPeopleDrinking =
-      "Mais pessoas bebendo do que rachando a conta!";
+      "Existem mais pessoas bebendo do que rachando/dividindo a conta!";
 
   /// Mensagem de erro para todos estarem bebendo quando ninguém está rachando a conta.
   static const String errorMsgPeopleDrinkingEveryone =
-      "Se todos estão bebendo, toques em \"Não\"!";
+      "Todos estão bebendo!? Desmarque a opção de \"Alguém bebendo\" e continue a divisão da conta.";
 
   /// Mensagem de erro para não haver ninguém bebendo quando alguém está rachando a conta.
   static const String errorMsgNoPeopleDrinking =
-      "Se não há ninguém bebendo, toques em \"Não\"!";
+      "Nenhuma pessoa bebendo!? Desmarque a opção de \"Alguém bebendo\".";
 
   /// Mensagem de erro para a quantidade de pessoas bebendo não ser informada.
   static const String errorMsgEmptyPeopleDrinking =
@@ -31,21 +31,22 @@ class CheckControllerErrorMessages {
 
   /// Mensagem de erro para o valor das bebidas ser maior do que o valor total da conta.
   static const String errorMsgTotalDrinkPrice =
-      "Valor das bebidas maior do que o valor total!";
+      "O valor das bebidas que você forneceu é maior do que o valor total da conta!";
 
   /// Mensagem de erro para o valor das bebidas ser igual ao valor total da conta.
   static const String errorMsgTotalDrinkPriceEqual =
-      "Valor das bebidas igual ao total, toque em \"Não\"";
+      "O valor das bebidas que você forneceu é igual ao valor total, ";
 
   /// Mensagem de erro para não haver valor informado para as bebidas quando alguém está rachando a conta.
   static const String errorMsgNoDrinkPrice =
-      "Se não há valor para bebidas toques em \"Não\"!";
+      "Valor das bebidas é obrigatório! Se não há alguém bebendo, desmarque a opção de \"Alguém bebendo\" ";
 
   /// Mensagem de erro para campos inválidos ou preenchidos incorretamente.
   static const String errorMsgInvalidFields = "Prencha os campos corretamente!";
 
-  static const String errorMsgFieldsMustBeFilled = "Os campos devem ser preenchidos";
+  static const String errorMsgFieldsMustBeFilled =
+      "Os campos devem ser preenchidos";
 
-  static const String numberOfPeopleCannotEqualToZero =  "❗️ A quantidade de pessoas não pode ser igual a zero!";
-  
+  static const String numberOfPeopleCannotEqualToZero =
+      "❗️ A quantidade de pessoas não pode ser igual a zero!";
 }
