dcbfbe3d68385115f7369d92d2b4ffe424c4a04e
Victor Pereira <victorruan135@gmail.com>
2024-09-09 15:20:34 -0300

    refactor: Remove unused code and update imports in total_value_screen.dart

--- lib/src/screens/total_value/total_value_screen.dart
+++ lib/src/screens/total_value/total_value_screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:racha_racha/src/core/widgets/custom_will_pop_scope_widget.dart';
 
 import '../../controllers/total_value_controller.dart';
-import '../../core/widgets/custom_drawer.dart';
 import '../../core/widgets/custom_floating_action_button.dart';
 import '../../core/widgets/custom_subtitle_text_widget.dart';
 import '../../core/widgets/custom_title_text_widget.dart';
@@ -40,42 +39,35 @@ class _TotalValueScreenState extends State<TotalValueScreen> {
   @override
   dispose() {
     super.dispose();
-    // isTotalValueFieldAutoFocusEnable = fDSalse;
     controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
     const String titleText = 'Para começar, digite o valor total da sua conta';
     const String subtitleText =
         '* Precisamos do valor do recibo para dar início à divisão da sua conta.';
     const String pathToNextPage = "/totalPeople";
     return CustomWillPopWidget(
       isExitedPaged: true,
-      drawer: const CustomDrawer(),
-      body: Padding(
-        padding: EdgeInsets.all(0.02 * size.height),
-        child: SafeArea(
-          child: Column(
-            children: [
-              const CustomTitleTextWidget(
-                titleText: titleText,
-              ),
-              SizedBox(height: size.height * 0.04),
-              Column(
-                children: [
-                  TotalValueField(
-                    controller: checkController,
-                    // autofocus: isTotalValueFieldAutoFocusEnable,
-                  ),
-                ],
-              ),
-              const CustomSubitleTextWidget(
-                subtitle: subtitleText,
-              ),
-            ],
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const CustomTitleTextWidget(
+              titleText: titleText,
+            ),
+            const SizedBox(height: 16),
+            Column(
+              children: [
+                TotalValueField(
+                  controller: checkController,
+                ),
+              ],
+            ),
+            const CustomSubitleTextWidget(
+              subtitle: subtitleText,
+            ),
+          ],
         ),
       ),
       floatingActionButton: const CustomFloatingActionButton(
