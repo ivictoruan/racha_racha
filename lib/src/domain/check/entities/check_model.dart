283c22910591673d7b662b6f64f4ff68ad48eb1f
Victor Pereira <victorruan135@gmail.com>
2024-09-11 09:50:33 -0300

    feat: adding CheckModel class.

---
+++ lib/src/domain/check/entities/check_model.dart
@@ -0,0 +1,23 @@
+class CheckModel {
+  double totalValue;
+  double individualPrice;
+  double waiterPercentage;
+  double totalWaiterValue;
+  bool isSomeoneDrinking;
+  double totalDrinkPrice;
+  int totalPeopleDrinking;
+  double individualPriceWhoIsDrinking;
+  int totalPeople;
+
+  CheckModel({
+    this.totalValue = 0,
+    this.individualPrice = 0,
+    this.waiterPercentage = 0,
+    this.totalWaiterValue = 0,
+    this.isSomeoneDrinking = false,
+    this.totalDrinkPrice = 0,
+    this.totalPeopleDrinking = 0,
+    this.individualPriceWhoIsDrinking = 0,
+    this.totalPeople = 1,
+  });
+}
