aad996236b84ff8083be0b56d2eacc8ab0f700fd
Victor Pereira <victorruan135@gmail.com>
2024-09-12 10:24:03 -0300

    feat: add unit tests for SpaceConstants.

---
+++ test/src/presenter/shared/ui/constants/space_constants_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:racha_racha/src/presenter/shared/ui/constants/space_constants.dart';
+
+void main() {
+  group('SpaceConstants', () {
+    test('extraSmall should be 8', () {
+      expect(SpaceConstants.extraSmall, equals(8.0));
+    });
+
+    test('small should be 12', () {
+      expect(SpaceConstants.small, equals(12.0));
+    });
+
+    test('medium should be 16', () {
+      expect(SpaceConstants.medium, equals(16.0));
+    });
+
+    test('screenBorder should be 20', () {
+      expect(SpaceConstants.screenBorder, equals(20.0));
+    });
+
+    test('large should be 28', () {
+      expect(SpaceConstants.large, equals(28.0));
+    });
+
+    test('extraLarge should be 56', () {
+      expect(SpaceConstants.extraLarge, equals(56.0));
+    });
+  });
+}
