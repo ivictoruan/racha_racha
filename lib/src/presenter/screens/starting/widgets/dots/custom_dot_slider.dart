3a4d51abb23939b23ef28f77f7eae6736ba776f5
Victor Pereira <victorruan135@gmail.com>
2024-09-12 17:19:48 -0300

    refactor: using custom spacing class.

--- lib/src/presenter/screens/starting/widgets/dots/custom_dot_slider.dart
+++ lib/src/presenter/screens/starting/widgets/dots/custom_dot_slider.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart' as carousel;
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart'
+    show AnimatedSmoothIndicator, ExpandingDotsEffect;
+
+import '../../../../shared/ui/constants/space_constants.dart';
 
 class CustomDotSplider extends StatelessWidget {
   final int activeIndex;
   final carousel.CarouselController controller;
   final int count;
 
-  const CustomDotSplider(
-      {super.key, required this.controller, required this.activeIndex, required this.count});
+  const CustomDotSplider({
+    super.key,
+    required this.controller,
+    required this.activeIndex,
+    required this.count,
+  });
+
+  void animateToSlide(int index) => controller.animateToPage(index);
 
   @override
-  Widget build(BuildContext context) {
-    void animateToSlide(int index) => controller.animateToPage(index);
-
-    return Center(
-      child: AnimatedSmoothIndicator(
-        onDotClicked: animateToSlide,
-        effect: const ExpandingDotsEffect(
-            dotWidth: 8,
-            dotHeight: 8,
+  Widget build(BuildContext context) => Center(
+        child: AnimatedSmoothIndicator(
+          onDotClicked: animateToSlide,
+          effect: const ExpandingDotsEffect(
+            dotWidth: SpaceConstants.extraSmall,
+            dotHeight: SpaceConstants.extraSmall,
             dotColor: Colors.deepPurple,
-            activeDotColor: Colors.deepPurple,),
-        activeIndex: activeIndex,
-        count: count,
-      ),
-    );
-  }
+            activeDotColor: Colors.deepPurple,
+          ),
+          activeIndex: activeIndex,
+          count: count,
+        ),
+      );
 }
