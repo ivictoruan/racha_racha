16f55a9b49c05b1faa5468f6b2785c26b980253f
Victor Pereira <victorruan135@gmail.com>
2024-09-12 11:56:31 -0300

    feat: add AppRouteManager class. The previous implementation of the routes in the Routes class has been deleted.

---
+++ lib/src/presenter/shared/ui/routes/app_route_manager.dart
@@ -0,0 +1,57 @@
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+import '../../../screens/total_value/controller/total_value_controller.dart';
+import '../../../screens/is_someone_drinking/is_someone_drinking_screen.dart';
+import '../../../screens/result/provider/result_screen_provider.dart';
+import '../../../screens/result/result_screen.dart';
+import '../../../screens/result/widgets/want_donate_widget.dart';
+import '../../../screens/settings/settings_screen.dart';
+import '../../../screens/starting/starting_screen.dart';
+import '../../../screens/total_people/total_people_screen.dart';
+import '../../../screens/total_value/total_value_screen.dart';
+
+class AppRouteManageranager {
+  static final GoRouter routes = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (_, __) => const StartingScreen(),
+      ),
+      GoRoute(
+        path: '/totalValue',
+        // TODO: criar uma classe para comportar este MultiProvider
+        builder: (_, __) => MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => TotalValueController(),
+            ),
+          ],
+          child: const TotalValueScreen(),
+        ),
+      ),
+      GoRoute(
+        path: '/totalPeople',
+        builder: (_, __) => const TotalPeopleScreen(),
+      ),
+      GoRoute(
+        path: '/isSomeoneDrinking',
+        builder: (_, __) => const IsSomeoneDrinkingScreen(),
+      ),
+      GoRoute(
+        path: '/result',
+        builder: (_, __) => const ResultScreenProvider(
+          child: ResultScreen(),
+        ),
+      ),
+      GoRoute(
+        path: '/settings',
+        builder: (_, __) => const SettingsScreen(),
+      ),
+      GoRoute(
+        path: '/wantDonate',
+        builder: (_, __) => const WantDonateWidget(),
+      ),
+    ],
+  );
+}
