ef7dc3214f007eeaed15becee6ef74b63f624edf
Victor Pereira <victorruan135@gmail.com>
2024-09-12 17:18:02 -0300

    refactor: Remove unused code and adjust spacing in IntroductionWidget

--- lib/src/presenter/screens/starting/widgets/introduction_widget.dart
+++ lib/src/presenter/screens/starting/widgets/introduction_widget.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart' as carousel;
+import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
 
+import '../../../shared/ui/constants/space_constants.dart';
 import 'contents/first_content_widget.dart';
 import 'contents/second_content_widget.dart';
-// import 'contents/third_content_widget.dart';
 import 'dots/custom_dot_slider.dart';
 
 class IntroductionWidget extends StatefulWidget {
@@ -18,29 +20,31 @@ class IntroductionWidget extends StatefulWidget {
 
 class _IntroductionWidgetState extends State<IntroductionWidget> {
   int activeIndex = 0;
-  final controller = carousel.CarouselController();
+  final carousel.CarouselController controller = carousel.CarouselController();
 
-  setActiveDot(index) {
-    setState(() {
-      activeIndex = index;
-    });
+  void setActiveDot({required int index}) {
+    activeIndex = index;
+    setState(() {});
   }
 
+  Size get size => MediaQuery.sizeOf(context);
+
+  final List<Widget> startingItems = [
+    const FirstContentWidget(),
+    const SecondContentWidget(),
+  ];
+
+  String get smartphoneLottie => "assets/lottie/smartphone.json";
+
+  double get lottieHeight => 200;
+  double get carouselHeight => 500;
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
-    const String smartphoneLottie = "assets/lottie/smartphone.json";
-
-    final List<Widget> startingItems = [
-      const FirstContentWidget(),
-      const SecondContentWidget(),
-      // ThirdContentWidget(), // Login/Registration
-    ];
     return Column(
       children: [
         Lottie.asset(
           smartphoneLottie,
-          height: size.height * 0.25,
+          height: 200,
         ),
         Stack(
           clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -49,27 +53,24 @@ class _IntroductionWidgetState extends State<IntroductionWidget> {
               carouselController: controller,
               options: carousel.CarouselOptions(
                 autoPlay: true,
-                height: MediaQuery.sizeOf(context).height * 0.75,
+                height: carouselHeight,
                 autoPlayInterval: const Duration(seconds: 15),
                 autoPlayAnimationDuration: const Duration(seconds: 4),
                 autoPlayCurve: Curves.fastLinearToSlowEaseIn,
-                onPageChanged: (index, reason) => setActiveDot(index),
+                onPageChanged: (int index, _) => setActiveDot(index: index),
                 scrollDirection: Axis.horizontal,
                 viewportFraction: 1.0,
               ),
               items: startingItems,
             ),
-            // const Spacer(),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.sizeOf(context).height * 0.55),
-                child: CustomDotSplider(
-                  activeIndex: activeIndex,
-                  controller: controller,
-                  count: startingItems.length,
-                ),
+            Positioned(
+              bottom: SpaceConstants.extraLarge,
+              left: 0,
+              right: 0,
+              child: CustomDotSplider(
+                activeIndex: activeIndex,
+                controller: controller,
+                count: startingItems.length,
               ),
             ),
           ],
