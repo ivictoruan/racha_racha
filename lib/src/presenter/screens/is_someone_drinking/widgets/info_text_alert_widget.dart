b96b0bf0338cd0dbb1fea10ee708f964349bb4c9
Victor Pereira <victorruan135@gmail.com>
2024-09-13 17:36:25 -0300

    refactor: add space constants and add comments.

--- lib/src/presenter/screens/is_someone_drinking/widgets/info_text_alert_widget.dart
+++ lib/src/presenter/screens/is_someone_drinking/widgets/info_text_alert_widget.dart
@@ -1,51 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:racha_racha/src/presenter/shared/controllers/check_controller.dart';
+import '../../../shared/constants/space_constants.dart';
+import '../../../shared/controllers/check_controller.dart';
 
 class InfoTextAlertWidget extends StatelessWidget {
   const InfoTextAlertWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.sizeOf(context);
-    return Material(
-      elevation: 0.5,
-      borderRadius: BorderRadius.circular(20), // 0.058 * size.width
-      color: Colors.white,
-      shadowColor: Colors.deepPurpleAccent,
-      child: SizedBox(
-        width: size.width * 0.85,
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: size.height * 0.004),
-          child: Consumer<CheckControllerImpl>(
-            builder: (context, controller, child) {
-              bool initialMsg =
-                  controller.msgError == "Digite o valor total da conta";
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.info,
-                    color: initialMsg == true ? Colors.green : Colors.red,
-                    size: 0.035 * size.width,
-                  ),
-                  SizedBox(
-                    width: 0.015 * size.width,
-                  ),
-                  Text(
-                    controller.msgError,
-                    style: TextStyle(
-                      color: initialMsg == true ? Colors.green : Colors.red,
-                      fontSize: 0.025 * size.width,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      // TODO: remover dependencia do controlador aqui
+      child: Consumer<CheckController>(
+        builder: (_, CheckController controller, __) {
+          bool initialMsg =
+              controller.msgError == "Digite o valor total da conta";
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.info,
+                color: initialMsg == true ? Colors.green : Colors.red,
+                size: 16,
+              ),
+              const SizedBox(
+                width: SpaceConstants.extraSmall,
+              ),
+              Text(
+                controller.msgError,
+                style: TextStyle(
+                  color: initialMsg == true ? Colors.green : Colors.red,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
