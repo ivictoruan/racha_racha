99679b5bb0673b5d40db8d7e0053997c69959bfc
Victor Pereira <victorruan135@gmail.com>
2024-10-25 17:44:21 -0300

    refactor: add restart as a button by side of share button and add history as a BottomNavigationBarItem.

--- lib/src/presenter/screens/result/check_result_screen.dart
+++ lib/src/presenter/screens/result/check_result_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../shared/constants/space_constants.dart';
+import '../../shared/utils/custom_utils.dart';
 import '../../shared/widgets/custom_will_pop_scope_widget.dart';
 import '../../../infra/services/generate_check_service.dart';
 import '../../../infra/services/share_check_service.dart';
@@ -23,10 +25,99 @@ class _CheckResultScreenState extends State<CheckResultScreen> {
     return CustomWillPopWidget(
       body: const ResultBodyWidget(),
       bottomNavigationBar: const CustomBottomNavBarWidget(),
-      floatingActionButton: SharedCheckOptionsWidget(
-        generateImageService: context.read<GenerateCheckService>(),
-        shareService: context.read<ShareCheckService>(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // TODO: componentizar como SecondaryIconButtonWidget
+          const SizedBox(width: SpaceConstants.small),
+
+          FilledButton.tonalIcon(
+            onPressed: () async =>
+                await _showModalRestartCheck(context: context),
+            icon: const Icon(
+              Icons.restart_alt_rounded,
+              color: Colors.grey,
+            ),
+            label: const Text(
+              "Reiniciar",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+          const SizedBox(width: SpaceConstants.small),
+          SharedCheckOptionsWidget(
+            generateImageService: context.read<GenerateCheckService>(),
+            shareService: context.read<ShareCheckService>(),
+          ),
+        ],
       ),
     );
   }
+
+  Future<void> _showModalRestartCheck({
+    required BuildContext context,
+  }) async =>
+      await showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        builder: (context) => Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                "Gostaria de recomeçar?",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: SpaceConstants.extraSmall),
+              Text(
+                "Se você não quer reiniciar toque em \"Não\"",
+                style: TextStyle(
+                  color: Colors.deepPurple[200],
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: SpaceConstants.extraSmall),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: SpaceConstants.medium),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FilledButton(
+                    onPressed: () => CustomUtils().goTo(
+                      "/totalValue",
+                      context,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                    ),
+                    child: const Text('Sim'),
+                  ),
+                  const SizedBox(width: SpaceConstants.extraSmall),
+                  TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('Não'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
 }
--- lib/src/presenter/screens/result/widgets/custom_bottom_nav_bar_widget.dart
+++ lib/src/presenter/screens/result/widgets/custom_bottom_nav_bar_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../shared/constants/space_constants.dart';
 import '../../../shared/utils/custom_utils.dart';
 
 class CustomBottomNavBarWidget extends StatelessWidget {
@@ -19,7 +18,8 @@ class CustomBottomNavBarWidget extends StatelessWidget {
             customUtils.goTo("/settings", context);
             break;
           case 2:
-            _showModalRestartCheck(context, CustomUtils());
+            customUtils.goTo("/history", context);
+
             break;
         }
       },
@@ -33,74 +33,10 @@ class CustomBottomNavBarWidget extends StatelessWidget {
           label: "Resultado",
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.restart_alt_rounded),
-          label: "Recomeçar",
+          icon: Icon(Icons.history),
+          label: "Histórico",
         ),
       ],
-    );
-  }
-
-  Future<void> _showModalRestartCheck(
-    BuildContext context,
-    CustomUtils customUtils,
-  ) async {
-    await showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      builder: (context) => Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              "Gostaria de recomeçar?",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: SpaceConstants.extraSmall),
-            Text(
-              "Se você não quer reiniciar toque em \"Não\"",
-              style: TextStyle(
-                color: Colors.deepPurple[200],
-                fontSize: 10,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: SpaceConstants.extraSmall),
-            const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: SpaceConstants.medium),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FilledButton(
-                  onPressed: () => customUtils.goTo("/totalValue", context),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                  ),
-                  child: const Text('Sim'),
-                ),
-                const SizedBox(width: SpaceConstants.extraSmall),
-                TextButton(
-                  onPressed: () => Navigator.of(context).pop(),
-                  child: const Text('Não'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
