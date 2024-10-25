77bb93c5808602ccb21a6dfa081f7b027cbbae91
Victor Pereira <victorruan135@gmail.com>
2024-10-25 11:07:54 -0300

    Update Flutter SDK version to stable and add packages.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\victo\fvm\versions\3.19.6"
+export "FLUTTER_ROOT=C:\Users\victo\fvm\versions\stable"
 export "FLUTTER_APPLICATION_PATH=C:\Users\victo\Documents\projects\mobile\flutter\racha_racha"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.5.5"
-export "FLUTTER_BUILD_NUMBER=4"
+export "FLUTTER_BUILD_NAME=1.5.6"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
