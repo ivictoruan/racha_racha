7292697f47e104041e0846bf762b7154d9d048db
Victor Pereira <victorruan135@gmail.com>
2024-10-27 10:24:14 -0300

    refactor: add CheckItemWidget and componentizing widgets.

--- lib/src/presenter/screens/history/history_screen.dart
+++ lib/src/presenter/screens/history/history_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../../domain/check/entities/check_model.dart';
 import '../../../external/services/firebase_check_database_service.dart';
+import '../../shared/widgets/floating_action_button_widget.dart';
+import '../../shared/widgets/loading_screen.dart';
+import '../result/widgets/custom_bottom_nav_bar_widget.dart';
+import 'widgets/check_item_widget.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({Key? key}) : super(key: key);
@@ -20,78 +25,73 @@ class _HistoryScreenState extends State<HistoryScreen> {
     _checksFuture = _fetchChecks();
   }
 
-  // TODO: levar isso para um controller
   Future<List<CheckModel>> _fetchChecks() async {
     final checkService = FirebaseCheckDatabaseService();
     return await checkService.getAllChecks();
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Histórico'),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<CheckModel>>(
-        future: _checksFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(
-                child: Text('Erro ao carregar os checks: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(
-              child: Text('Faça sua primeira divisão!'),
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: FutureBuilder<List<CheckModel>>(
+          future: _checksFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const LoadingScreen();
+            } else if (snapshot.hasError) {
+              return Center(
+                  child: Text('Erro ao carregar os checks: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return _buildEmptyWidget(context);
+            }
+
+            final checks = snapshot.data!.toList();
+
+            return ListView.builder(
+              itemCount: checks.length,
+              itemBuilder: (context, index) {
+                final check = checks[index];
+                final checkIndex = checks.length - index;
+                return CheckItemWidget(
+                  check: check,
+                  index: checkIndex,
+                );
+              },
             );
-          }
+          },
+        ),
+        floatingActionButton: FloatingActionButtonWidget(
+          onPressed: () => context.go('/totalValue'),
+          // TODO: desativar o botão quando estiver em loading
+          isActivated: true,
+          icon: Icons.add,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: const CustomBottomNavBarWidget(
+          isFinishingCheck: false,
+        ),
+      );
 
-          final checks = snapshot.data!.toList();
-
-          return ListView.builder(
-            itemCount: checks.length,
-            itemBuilder: (context, index) {
-              final check = checks[index];
-
-              // TODO: melhorar este ListTile (componentizar)
-              return ListTile(
-                title: Text('Check ${index + 1}: ${check.creationDate}'),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Total: R\$ ${check.totalValue.toStringAsFixed(2)}'),
-                    Text(
-                        'Preço Individual: R\$ ${check.individualPrice.toStringAsFixed(2)}'),
-                    Text(
-                        'Porcentagem do Garçom: ${check.waiterPercentage.toStringAsFixed(2)}%'),
-                    Text(
-                        'Total do Garçom: R\$ ${check.totalWaiterValue.toStringAsFixed(2)}'),
-                    Text(
-                        'Alguém Bebendo: ${check.isSomeoneDrinking ? "Sim" : "Não"}'),
-                    Text(
-                        'Total de Bebidas: R\$ ${check.totalDrinkPrice.toStringAsFixed(2)}'),
-                    Text(
-                        'Total de Pessoas Bebendo: ${check.totalPeopleDrinking}'),
-                    Text(
-                        'Preço Individual Bebendo: R\$ ${check.individualPriceWhoIsDrinking.toStringAsFixed(2)}'),
-                    Text('Total de Pessoas: ${check.totalPeople}'),
-                    Text(
-                        'Dia da Criação: ${check.creationDate!.day.toString()}/${check.creationDate!.month.toString()}/${check.creationDate!.year.toString()} às ${check.creationDate!.hour.toString()}:${check.creationDate!.minute.toString()}'),
-                  ],
-                ),
-                isThreeLine: true,
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.go('/totalValue');
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  Column _buildEmptyWidget(BuildContext context) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Lottie.asset(
+              'assets/lottie/empty.json',
+              height: MediaQuery.sizeOf(context).height * 0.45,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              "Você ainda não fez nenhuma divisão! Para começar toque no botão abaixo!",
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium!
+                  .copyWith(color: Colors.deepPurple),
+            ),
+          ),
+        ],
+      );
 }
