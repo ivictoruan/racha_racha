089cfcd19369ac074c07915e24c7a92bf45c9236
Victor Pereira <victorruan135@gmail.com>
2024-10-29 08:29:36 -0300

    Refactor: Update return type of getAllChecks() in CheckDatabaseService

--- lib/src/infra/services/database/check_database_service.dart
+++ lib/src/infra/services/database/check_database_service.dart
@@ -3,7 +3,7 @@ import '../../../domain/check/entities/check_model.dart';
 abstract class CheckDatabaseService {
   Future<void> createCheck({required CheckModel check});
   Future<CheckModel?> getCheckById({required String checkId});
-  Future<List<CheckModel?>> getAllChecks();
+  Future<List<CheckModel>>? getAllChecks();
   Future<void> updateCheck({
     required String checkId,
     required CheckModel check,
