2134ec02eef78d9b7c36bb2d129c721eaa4bdccc
Victor Pereira <victorruan135@gmail.com>
2024-10-29 08:44:02 -0300

    Refactor: - Renamed the `isActivated` property to `isEnabled` for clarity. - Updated the button color and elevation based on the `isEnabled` property. - Added animation and box shadow to the custom FAB widget.

--- lib/src/presenter/shared/widgets/floating_action_button_widget.dart
+++ lib/src/presenter/shared/widgets/floating_action_button_widget.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
 
-class FloatingActionButtonWidget extends StatefulWidget {
+class FloatingActionButtonWidget extends StatelessWidget {
   final void Function()? onPressed;
-  final bool isActivated;
+  final bool isEnabled;
   final IconData? icon;
 
   const FloatingActionButtonWidget({
     Key? key,
     required this.onPressed,
-    required this.isActivated,
+    required this.isEnabled,
     this.icon,
   }) : super(key: key);
 
   @override
-  State<FloatingActionButtonWidget> createState() =>
-      _FloatingActionButtonWidgetState();
-}
-
-class _FloatingActionButtonWidgetState
-    extends State<FloatingActionButtonWidget> {
-  @override
-  Widget build(BuildContext context) => FloatingActionButton(
-        elevation: elevation,
-        focusElevation: focusElevation,
-        backgroundColor: backgroundColor,
-        onPressed: widget.isActivated ? widget.onPressed : null,
-        child: Icon(
-          icon,
-          size: 32,
-          color: Colors.white,
+  Widget build(BuildContext context) => AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: backgroundColor,
+          boxShadow: [
+            BoxShadow(
+              color: isEnabled
+                  ? Colors.deepPurple.withOpacity(0.5)
+                  : Colors.transparent,
+              blurRadius: elevation,
+              offset: Offset(0, elevation),
+              spreadRadius: 2,
+            ),
+          ],
+        ),
+        child: FloatingActionButton(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          splashColor: Colors.white70,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          onPressed: isEnabled ? onPressed : null,
+          child: Icon(
+            iconData,
+            size: 32,
+            color: Colors.white,
+          ),
         ),
       );
 
@@ -37,11 +50,11 @@ class _FloatingActionButtonWidgetState
   Color get activatedButtonColor => Colors.deepPurple;
 
   Color get backgroundColor =>
-      widget.isActivated ? Colors.deepPurple : disabledButtonColor;
+      isEnabled ? Colors.deepPurple : disabledButtonColor;
 
-  IconData get icon => widget.icon ?? Icons.arrow_forward;
+  IconData get iconData => icon ?? Icons.arrow_forward;
 
-  double get elevation => widget.isActivated ? 2 : 0;
+  double get elevation => isEnabled ? 2 : 0;
 
   double get focusElevation => 2;
 }
