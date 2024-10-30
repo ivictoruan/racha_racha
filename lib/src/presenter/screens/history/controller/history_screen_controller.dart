74bfab5f5e12c5a7f92d4afdb03e47b3bfd19c0a
Victor Pereira <victorruan135@gmail.com>
2024-10-30 16:34:50 -0300

    refactor: rename HistoryController to HistoryScreenController and improve checks fetching logic

--- lib/src/presenter/screens/history/controller/history_screen_controller.dart
+++ lib/src/presenter/screens/history/controller/history_screen_controller.dart
@@ -3,23 +3,29 @@ import 'package:flutter/material.dart';
 import '../../../../domain/check/entities/check_model.dart';
 import '../../../../infra/services/database/check_database_service.dart';
 
-class HistoryController with ChangeNotifier {
+class HistoryScreenController with ChangeNotifier {
   final CheckDatabaseService checkService;
 
-  HistoryController({required this.checkService});
+  HistoryScreenController({required this.checkService});
 
   bool _loading = false;
   bool get loading => _loading;
 
-  Future<List<CheckModel>> fetchChecks() async {
+  List<CheckModel> _checks = [];
+  List<CheckModel> get checks => _checks;
+
+  Future<void> fetchChecks() async {
     _loading = true;
     notifyListeners();
     try {
-      final checks = await checkService.getAllChecks();
-      return Future.value(checks);
+      _checks = await checkService.getAllChecks()!;
     } finally {
       _loading = false;
       notifyListeners();
     }
+  }
+
+  Future<void> reloadChecks() async {
+    await fetchChecks();
   }
 }
