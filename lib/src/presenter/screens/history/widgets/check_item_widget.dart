219c6cc279c9cc5f5578a8adaf0dc5774c75cc3f
Victor Pereira <victorruan135@gmail.com>
2024-10-30 15:59:29 -0300

    fix: adjusting wrong index (index was starting with 2)

--- lib/src/presenter/screens/history/widgets/check_item_widget.dart
+++ lib/src/presenter/screens/history/widgets/check_item_widget.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 
 import '../../../../domain/check/entities/check_model.dart';
 import '../../../shared/constants/space_constants.dart';
+import '../../../shared/routes/app_route_manager.dart';
 
 class CheckItemWidget extends StatelessWidget {
   final CheckModel check;
@@ -27,57 +28,66 @@ class CheckItemWidget extends StatelessWidget {
             borderRadius: BorderRadius.circular(SpaceConstants.medium),
           ),
           elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(SpaceConstants.medium),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(Icons.date_range_outlined),
-                    const SizedBox(width: SpaceConstants.small),
-                    Text(
-                      'Divisão ${index + 1} - $dateFormatted',
-                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: SpaceConstants.extraSmall),
-                Divider(color: Colors.deepPurple[200]),
-                const SizedBox(height: SpaceConstants.extraSmall),
-                _buildRow(
-                  icon: Icons.attach_money,
-                  label: 'Total:',
-                  value: 'R\$ ${check.totalValue.toStringAsFixed(2)}',
-                  context: context,
-                ),
-                _buildRow(
-                  icon: Icons.person,
-                  label: 'Pessoas:',
-                  value: '${check.totalPeople}',
-                  context: context,
-                ),
-                _buildRow(
-                  icon: Icons.percent,
-                  label: 'Porcentagem Garçom:',
-                  value: '${check.waiterPercentage.toStringAsFixed(2)}%',
-                  context: context,
-                ),
-                if (check.isSomeoneDrinking) ...[
+          
+          child: InkWell(
+            borderRadius: const BorderRadius.all(Radius.circular(15)),
+            // TODO: ajustar tela de resultado para receber um check (aproveitar isFinishing)
+            // onTap: () {
+            //   Navigator.of(context).pushNamed(AppRouteManager.result);
+            // },
+            child: Padding(
+              padding: const EdgeInsets.all(SpaceConstants.medium),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.date_range_outlined),
+                      const SizedBox(width: SpaceConstants.small),
+                      Text(
+                        'Divisão $index - $dateFormatted',
+                        style:
+                            Theme.of(context).textTheme.titleMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                      ),
+                    ],
+                  ),
                   const SizedBox(height: SpaceConstants.extraSmall),
                   Divider(color: Colors.deepPurple[200]),
                   const SizedBox(height: SpaceConstants.extraSmall),
                   _buildRow(
-                    icon: Icons.local_drink,
-                    label: 'Total Bebidas:',
-                    value: 'R\$ ${check.totalDrinkPrice.toStringAsFixed(2)}',
+                    icon: Icons.attach_money,
+                    label: 'Total:',
+                    value: 'R\$ ${check.totalValue.toStringAsFixed(2)}',
                     context: context,
                   ),
+                  _buildRow(
+                    icon: Icons.person,
+                    label: 'Pessoas:',
+                    value: '${check.totalPeople}',
+                    context: context,
+                  ),
+                  _buildRow(
+                    icon: Icons.percent,
+                    label: 'Porcentagem Garçom:',
+                    value: '${check.waiterPercentage.toStringAsFixed(2)}%',
+                    context: context,
+                  ),
+                  if (check.isSomeoneDrinking) ...[
+                    const SizedBox(height: SpaceConstants.extraSmall),
+                    Divider(color: Colors.deepPurple[200]),
+                    const SizedBox(height: SpaceConstants.extraSmall),
+                    _buildRow(
+                      icon: Icons.local_drink,
+                      label: 'Total Bebidas:',
+                      value: 'R\$ ${check.totalDrinkPrice.toStringAsFixed(2)}',
+                      context: context,
+                    ),
+                  ],
                 ],
-              ],
+              ),
             ),
           ),
         ),
