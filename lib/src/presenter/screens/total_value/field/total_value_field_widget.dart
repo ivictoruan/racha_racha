72f5e67ed4b631128826c962374c67bd971ec8f1
Victor Pereira <victorruan135@gmail.com>
2024-10-30 16:00:35 -0300

    refactor: make controller required in TotalValueFieldWidget constructor

--- lib/src/presenter/screens/total_value/field/total_value_field_widget.dart
+++ lib/src/presenter/screens/total_value/field/total_value_field_widget.dart
@@ -5,23 +5,24 @@ import '../../../shared/input_formatters/currency_text_input_formatter.dart';
 import '../../../shared/widgets/text_form_field_widget.dart';
 
 class TotalValueFieldWidget extends StatelessWidget {
-  final void Function(String)? onChanged;
-
-  final dynamic Function(String)? onFieldSubmitted;
-
   final bool? autofocus;
+  final TextEditingController controller;
+  final void Function(String)? onChanged;
+  final dynamic Function(String)? onFieldSubmitted;
 
   const TotalValueFieldWidget({
     Key? key,
-    this.autofocus,
+    required this.controller,
     this.onChanged,
     this.onFieldSubmitted,
+    this.autofocus,
   }) : super(key: key);
 
-// TODO: transformar num método de build e deixar emTotalValueScreen
+// TODO: transformar num método de build e deixar emTotalValueScreen1
   @override
   Widget build(BuildContext context) => TextFormFieldWidget(
         hintText: 'R\$ 0,00',
+        controller: controller,
         inputFormatters: <TextInputFormatter>[
           CurrencyTextInputFormatter(),
         ],
