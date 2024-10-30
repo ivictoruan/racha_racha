0dc7e06c2ff93dbc2e873c2ead64c1cbf3ee54f4
Victor Pereira <victorruan135@gmail.com>
2024-10-30 15:55:59 -0300

    refactor: remove GoRouter dependency from CustomUtils for cleaner navigation handling

--- lib/src/presenter/shared/utils/custom_utils.dart
+++ lib/src/presenter/shared/utils/custom_utils.dart
@@ -2,28 +2,10 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:go_router/go_router.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class CustomUtils {
-  void goTo(String route, BuildContext context) {
-    try {
-      GoRouter.of(context).push(route);
-    } catch (e) {
-      log('Não foi possível ir para a rota $route');
-    }
-  }
-
-  void goToBack(BuildContext context) {
-    try {
-      GoRouter.of(context).pop();
-    } catch (e) {
-      log('Não foi possível voltar para página anterior');
-    }
-  }
-
   void exitApp() {
     var platform = debugDefaultTargetPlatformOverride;
     if (platform == TargetPlatform.android) {
@@ -32,7 +14,6 @@ class CustomUtils {
       exit(0);
     }
   }
-
 
   Future goToUrl(String url) async {
     try {
