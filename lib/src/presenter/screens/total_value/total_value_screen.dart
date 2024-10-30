d5cf7fe724bbf8050c6bc825b6afad70f7635fe8
Victor Pereira <victorruan135@gmail.com>
2024-10-30 16:02:21 -0300

    refactor: update TotalValueScreen to use TextEditingController and improve navigation

--- lib/src/presenter/screens/total_value/total_value_screen.dart
+++ lib/src/presenter/screens/total_value/total_value_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
 
-import '../../shared/utils/custom_utils.dart';
+import '../../shared/routes/app_route_manager.dart';
 import '../../shared/widgets/title_text_widget.dart';
 import '../../shared/constants/space_constants.dart';
 import '../../shared/widgets/floating_action_button_widget.dart';
 import '../../shared/widgets/subtitle_text_widget.dart';
-import '../../shared/widgets/custom_will_pop_scope_widget.dart';
+import '../../shared/widgets/will_pop_scope_widget.dart';
 import '../../shared/controllers/check_controller.dart';
 import 'field/total_value_field_widget.dart';
 
@@ -18,17 +19,37 @@ class TotalValueScreen extends StatefulWidget {
 }
 
 class _TotalValueScreenState extends State<TotalValueScreen> {
-  void restartData() {
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await context.read<CheckController>().restartCheck();
+  late TextEditingController _textController;
+  void init() {
+    _textController = TextEditingController();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        final controller = context.read<CheckController>();
+
+        _textController.text = controller.totalCheckPrice;
+
+        controller.restartCheck();
+
+        controller.addListener(() {
+          if (mounted) {
+            _textController.text = controller.totalCheckPrice;
+          }
+        });
+      }
     });
   }
 
   @override
-  initState() {
+  void initState() {
     super.initState();
+    init();
+  }
 
-    restartData();
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 
   String get titleText => 'Para começar, digite o valor total da sua conta.';
@@ -36,49 +57,41 @@ class _TotalValueScreenState extends State<TotalValueScreen> {
   String get subtitleText =>
       '* Precisamos do valor do recibo para dar início à divisão da sua conta.';
 
-  String get pathToNextPage => "/totalPeople";
-
   @override
   Widget build(BuildContext context) {
     final CheckController controller = context.watch<CheckController>();
 
-    return CustomWillPopWidget(
-      // TODO: remover WillPop?
-      isExitedPaged: true,
-      body: SafeArea(
-        child: Column(
-          children: [
-            // TODO: implantar TitleTextWidget em TotalValueFieldWidget de e expor
-            /// ´titleText´ como parâmetro
-            TitleTextWidget(
-              titleText: titleText,
-            ),
-            const SizedBox(height: SpaceConstants.medium),
-            Column(
-              children: [
-                TotalValueFieldWidget(
-                  onChanged: (String newTotalCheckValue) {
-                    controller.totalCheckPrice = newTotalCheckValue;
-                  },
-                  onFieldSubmitted: (String newTotalCheckValue) {
-                    CustomUtils customUtils = CustomUtils();
-                    controller.totalCheckPrice = newTotalCheckValue;
-                    bool isValid = controller.check.totalValue > 0;
-                    isValid ? customUtils.goTo("/totalPeople", context) : null;
-                  },
-                ),
-              ],
-            ),
-            // TODO: implantar CustomSubitleTextWidget em TotalValueFieldWidget de e expor ´titleText´ como parâmetro
-            SubitleTextWidget(
-              subtitle: subtitleText,
-            ),
-          ],
-        ),
+    return WillPopScopeWidget(
+      onYesPressed: () => controller.restartCheck(),
+      body: Column(
+        children: [
+          TitleTextWidget(
+            titleText: titleText,
+          ),
+          const SizedBox(height: SpaceConstants.medium),
+          TotalValueFieldWidget(
+            controller: _textController,
+            onChanged: (String newTotalCheckValue) {
+              controller.totalCheckPrice = newTotalCheckValue;
+            },
+            onFieldSubmitted: (String newTotalCheckValue) {
+              controller.totalCheckPrice = newTotalCheckValue;
+              bool isValid = controller.check.totalValue > 0;
+              isValid
+                  ? Navigator.of(context).pushNamed(
+                      AppRouteManager.totalPeople,
+                    )
+                  : null;
+            },
+          ),
+          SubitleTextWidget(subtitle: subtitleText),
+        ],
       ),
       floatingActionButton: FloatingActionButtonWidget(
-        onPressed: () => CustomUtils().goTo(pathToNextPage, context),
-        isActivated: controller.state == CheckState.totalCheckValueValid,
+        onPressed: () => Navigator.of(context).pushNamed(
+          AppRouteManager.totalPeople,
+        ),
+        isEnabled: controller.state == CheckState.totalCheckValueValid,
       ),
     );
   }
