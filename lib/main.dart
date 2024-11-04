e89af91076e0aaa29a6b2b98fd1af6cd86806d1e
Victor Pereira <victorruan135@gmail.com>
2024-11-04 10:32:39 -0300

    refactor: rename CustomProvider to GeneralWidgetProvider in and adjusting initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'src/app_starter.dart';
 import 'src/app_widget.dart';
-import 'src/presenter/shared/providers/custom_provider.dart';
+import 'src/presenter/shared/providers/general_widget_provider.dart';
 
-void main() async {
+Future<void> main() async {
   await AppStarter.initApp();
-
   runApp(
-    const CustomProvider(
+    const GeneralWidgetProvider(
       child: RachaRachaApp(),
     ),
   );
