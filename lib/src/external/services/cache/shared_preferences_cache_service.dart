9e2e373540bee35d14c1d613b4a4a28a5714bddd
Victor Pereira <victorruan135@gmail.com>
2024-11-06 16:22:50 -0300

    feat: implement SharedPreferencesCacheService for data caching

---
+++ lib/src/external/services/cache/shared_preferences_cache_service.dart
@@ -0,0 +1,55 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../infra/services/cache/cache_service.dart';
+
+class SharedPreferencesCacheService extends CacheService {
+  @override
+  Future<void> saveData<T>(String key, T value) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (value is String) {
+      await prefs.setString(key, value);
+    } else if (value is int) {
+      await prefs.setInt(key, value);
+    } else if (value is bool) {
+      await prefs.setBool(key, value);
+    } else if (value is double) {
+      await prefs.setDouble(key, value);
+    } else if (value is List<String>) {
+      await prefs.setStringList(key, value);
+    } else {
+      throw UnsupportedError("Type ${T.runtimeType} is not supported");
+    }
+  }
+
+  @override
+  Future<T?> getData<T>(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (T == String) {
+      return prefs.getString(key) as T?;
+    } else if (T == int) {
+      return prefs.getInt(key) as T?;
+    } else if (T == bool) {
+      return prefs.getBool(key) as T?;
+    } else if (T == double) {
+      return prefs.getDouble(key) as T?;
+    } else if (T == List<String>) {
+      return prefs.getStringList(key) as T?;
+    } else {
+      throw UnsupportedError("Type ${T.runtimeType} is not supported");
+    }
+  }
+
+  @override
+  Future<void> removeData(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
+  }
+
+  @override
+  Future<void> clearCache() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+}
