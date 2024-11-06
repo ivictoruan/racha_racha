805d324e3b4eec6cab51fb880b160652ae452741
Victor Pereira <victorruan135@gmail.com>
2024-11-06 16:25:25 -0300

    feat: refactor IsSomeoneDrinkingScreen to simplify onYesPressed logic and improve modal display

--- lib/src/presenter/screens/is_someone_drinking/is_someone_drinking_screen.dart
+++ lib/src/presenter/screens/is_someone_drinking/is_someone_drinking_screen.dart
@@ -73,112 +73,104 @@ class _IsSomeoneDrinkingScreenState extends State<IsSomeoneDrinkingScreen> {
     setState(() {});
   }
 
-  void onYesPressed() async {
-    await controller.restartCheck();
-
-    if (mounted) {
-      Navigator.pop(context, true);
-    }
-  }
+  void onYesPressed() async {}
 
   Future _showModalWhotIsNotDrinking(
     BuildContext context,
-  ) async {
-    await showModalBottomSheet(
-      context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      // TODO: componentizar este "Modal"
-      builder: (context) => Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              "Todas informações estão corretas?",
-              style: TextStyle(
-                color: Colors.deepPurple,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: SpaceConstants.extraSmall),
-            Consumer<CheckController>(
-              builder: (context, controller, child) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ConfirmInfoWidget(
-                      startText: "Total da conta: ",
-                      endText: "R\$ ${controller.totalCheckPrice}",
-                    ),
-                    const SizedBox(height: 8),
-                    ConfirmInfoWidget(
-                      startText: "Quantidade de pessoas: ",
-                      endText: controller.totalPeople.toString(),
-                    ),
-                    const SizedBox(height: 8),
-                    ConfirmInfoWidget(
-                      startText: "Gorjeta/Garçom: ",
-                      endText: "${controller.waiterPercentage.toString()} %",
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FilledButton(
-                  onPressed: () {
-                    final controller = context.read<CheckController>();
-                    controller.calculateCheckResult();
-                    controller.isSomeoneDrinking = false;
-
-                    final Map<String, Object> argumentsToHistoryScreen = {
-                      'isFinishing': true,
-                      'check': controller.check,
-                    };
-
-                    Navigator.of(context).pushNamed(
-                      AppRouteManager.result,
-                      arguments: argumentsToHistoryScreen,
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                  ),
-                  child: const Text('Sim'),
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  onPressed: () {
-                    controller.isSomeoneDrinking = true;
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('Não'),
-                ),
-                const SizedBox(width: 8),
-                TextButton.icon(
-                  onPressed: () {
-                    controller.restartCheck();
-                    // TODO! rever essa forma de voltar para tela de valor total!
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.restart_alt_rounded),
-                  label: const Text("Reiniciar"),
-                ),
-              ],
-            ),
-          ],
+  ) async =>
+      await showModalBottomSheet(
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
         ),
-      ),
-    );
-  }
+        // TODO: componentizar este "Modal"
+        builder: (context) => Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                "Todas informações estão corretas?",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: SpaceConstants.extraSmall),
+              Consumer<CheckController>(
+                builder: (context, controller, child) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ConfirmInfoWidget(
+                        startText: "Total da conta: ",
+                        endText: "R\$ ${controller.totalValue}",
+                      ),
+                      const SizedBox(height: 8),
+                      ConfirmInfoWidget(
+                        startText: "Quantidade de pessoas: ",
+                        endText: controller.totalPeople.toString(),
+                      ),
+                      const SizedBox(height: 8),
+                      ConfirmInfoWidget(
+                        startText: "Gorjeta/Garçom: ",
+                        endText: "${controller.waiterPercentage.toString()} %",
+                      ),
+                      const SizedBox(height: 16),
+                    ],
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FilledButton(
+                    onPressed: () {
+                      final controller = context.read<CheckController>();
+                      controller.calculateCheckResult();
+                      controller.isSomeoneDrinking = false;
+
+                      final Map<String, Object> argumentsToHistoryScreen = {
+                        'isFinishing': true,
+                        'check': controller.check,
+                      };
+
+                      Navigator.of(context).pushNamed(
+                        AppRouteManager.result,
+                        arguments: argumentsToHistoryScreen,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                    ),
+                    child: const Text('Sim'),
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton(
+                    onPressed: () {
+                      controller.isSomeoneDrinking = true;
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Não'),
+                  ),
+                  const SizedBox(width: 8),
+                  TextButton.icon(
+                    onPressed: () {
+                      // TODO! rever essa forma de voltar para tela de valor total!
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.restart_alt_rounded),
+                    label: const Text("Reiniciar"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
 }
