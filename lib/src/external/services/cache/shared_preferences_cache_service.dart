64031a2eb4c4050a3af7f3529da9261ede414686
Victor Pereira <victorruan135@gmail.com>
2024-11-12 10:12:43 -0300

    fix: adjusting initializing.

--- lib/src/external/services/cache/shared_preferences_cache_service.dart
+++ lib/src/external/services/cache/shared_preferences_cache_service.dart
@@ -1,59 +1,48 @@
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../../infra/services/cache/cache_service.dart';
-
 class SharedPreferencesCacheService extends CacheService {
-  late final SharedPreferences _prefs;
-
-  SharedPreferencesCacheService() {
-    _initialize();
-  }
-
-  Future<void> _initialize() async {
-    _prefs = await SharedPreferences.getInstance();
-  }
-
   @override
   Future<void> saveData<T>(String key, T value) async {
+    final prefs = await SharedPreferences.getInstance();
     if (value is String) {
-      await _prefs.setString(key, value);
+      await prefs.setString(key, value);
     } else if (value is int) {
-      await _prefs.setInt(key, value);
+      await prefs.setInt(key, value);
     } else if (value is bool) {
-      await _prefs.setBool(key, value);
+      await prefs.setBool(key, value);
     } else if (value is double) {
-      await _prefs.setDouble(key, value);
+      await prefs.setDouble(key, value);
     } else if (value is List<String>) {
-      await _prefs.setStringList(key, value);
+      await prefs.setStringList(key, value);
     } else {
       throw UnsupportedError("Type ${T.runtimeType} is not supported");
     }
   }
-
   @override
   Future<T?> getData<T>(String key) async {
+    final prefs = await SharedPreferences.getInstance();
     if (T == String) {
-      return _prefs.getString(key) as T?;
+      return prefs.getString(key) as T?;
     } else if (T == int) {
-      return _prefs.getInt(key) as T?;
+      return prefs.getInt(key) as T?;
     } else if (T == bool) {
-      return _prefs.getBool(key) as T?;
+      return prefs.getBool(key) as T?;
     } else if (T == double) {
-      return _prefs.getDouble(key) as T?;
+      return prefs.getDouble(key) as T?;
     } else if (T == List<String>) {
-      return _prefs.getStringList(key) as T?;
+      return prefs.getStringList(key) as T?;
     } else {
       throw UnsupportedError("Type ${T.runtimeType} is not supported");
     }
   }
-
   @override
   Future<void> removeData(String key) async {
-    await _prefs.remove(key);
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(key);
   }
-
   @override
   Future<void> clearCache() async {
-    await _prefs.clear();
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
   }
 }
