d8910414076448d2d34c93bb879036398db43edc
Victor Pereira <victorruan135@gmail.com>
2024-11-15 16:38:59 -0300

    feat: add providers for GenerateCheckService and ShareCheckService in HistoryScreenProvider

--- lib/src/presenter/screens/history/provider/history_screen_provider.dart
+++ lib/src/presenter/screens/history/provider/history_screen_provider.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../external/services/generate_check_service_impl.dart';
+import '../../../../external/services/share_plus_service_impl.dart';
 import '../../../../infra/services/database/check_database_service.dart';
+import '../../../../infra/services/generate_check_service.dart';
+import '../../../../infra/services/share_check_service.dart';
 import '../controller/history_screen_controller.dart';
 
 class HistoryScreenProvider extends StatelessWidget {
@@ -11,6 +15,12 @@ class HistoryScreenProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MultiProvider(
         providers: [
+          Provider<GenerateCheckService>(
+            create: (_) => GenerateCheckServiceImpl(),
+          ),
+          Provider<ShareCheckService>(
+            create: (_) => SharePlusCheckServiceImpl(),
+          ),
           ChangeNotifierProvider(
             create: (context) => HistoryScreenController(
               checkService: context.read<CheckDatabaseService>(),
