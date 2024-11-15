fbd91044374a963c68eb2d4fbdd24ed145d62681
Victor Pereira <victorruan135@gmail.com>
2024-11-15 16:37:15 -0300

    feat: add BoxShadowWidget for customizable shadow effects

---
+++ lib/src/presenter/shared/widgets/box_shadow_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../constants/space_constants.dart';
+
+class BoxShadowWidget extends StatelessWidget {
+  final Widget child;
+  final bool isExpaded;
+  const BoxShadowWidget({
+    Key? key,
+    required this.child,
+    this.isExpaded = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Material(
+        elevation: isExpaded ? 0 : 2,
+        shadowColor: Colors.deepPurpleAccent,
+        color: Colors.white,
+        borderRadius: isExpaded
+            ? const BorderRadius.only(
+                bottomLeft: Radius.circular(SpaceConstants.medium),
+                topLeft: Radius.circular(SpaceConstants.medium),
+              )
+            : BorderRadius.circular(SpaceConstants.medium),
+        child: child,
+      );
+}
