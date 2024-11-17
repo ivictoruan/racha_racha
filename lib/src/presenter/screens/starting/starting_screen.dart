b2ffcd2a3b4a592695e38761582aca1d06221d3b
Victor Pereira <victorruan135@gmail.com>
2024-11-17 09:43:34 -0300

    refactor: removing inicialization logic and moving it to FirstScreen.

--- lib/src/presenter/screens/starting/starting_screen.dart
+++ lib/src/presenter/screens/starting/starting_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:provider/provider.dart';
 
-import '../../../infra/services/database/auth_service.dart';
-import '../../../infra/services/cache/cache_service.dart';
 import '../../shared/routes/app_route_manager.dart';
 import '../../shared/constants/app_assets.dart';
-import '../../shared/controllers/user_controller.dart';
 import '../../shared/widgets/will_pop_scope_widget.dart';
 import '../../shared/constants/space_constants.dart';
 import 'widgets/go_to_rachar_button_widget.dart';
@@ -19,68 +14,7 @@ class StartingScreen extends StatefulWidget {
   State<StartingScreen> createState() => _StartingScreenState();
 }
 
-class _StartingScreenState extends State<StartingScreen>
-    with SingleTickerProviderStateMixin {
-  void _initializeUserSession() {
-    SchedulerBinding.instance.addPostFrameCallback((_) async {
-      if (!mounted) return;
-
-      final userController = context.read<UserController>();
-      final authService = context.read<AuthService>();
-      final cacheService = context.read<CacheService>();
-
-      await _setUserId(
-        userController: userController,
-        authService: authService,
-        cacheService: cacheService,
-      );
-
-      await _handleAppStart(cacheService: cacheService);
-    });
-  }
-
-  Future<void> _setUserId({
-    required UserController userController,
-    required AuthService authService,
-    required CacheService cacheService,
-  }) async {
-    final userId = await cacheService.getData<String>('userId');
-
-    if (userId == null || userId.isEmpty) {
-      userController.id = await authService.signInAnonymously();
-      await cacheService.saveData<String>('userId', userController.id);
-    } else {
-      userController.id = userId;
-    }
-  }
-
-  Future<void> _handleAppStart({required CacheService cacheService}) async {
-    final alreadyStarted =
-        await cacheService.getData<bool>('alreadyStartsApp') ?? false;
-
-    if (!alreadyStarted) {
-      await cacheService.saveData<bool>('alreadyStartsApp', true);
-    } else {
-      _navigateToHistory();
-    }
-  }
-
-  void _navigateToHistory() {
-    if (mounted) {
-      Navigator.pushReplacementNamed(context, AppRouteManager.history);
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    /// TODO: esta inicialização deve estar em outra tela (como FirstRoute ou FirstScreen)
-    /// TODO: levar essa função para essa tela e ela deve levar para a tela de histórico ou para esta te starting
-    /// !FIX: está aparecendo por alguns ms na tela ao redirecionar!
-    _initializeUserSession();
-  }
-
+class _StartingScreenState extends State<StartingScreen> {
   @override
   Widget build(BuildContext context) => WillPopScopeWidget(
         onYesPressed: () => Navigator.pop(context, true),
