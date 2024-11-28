37abb1c295c0e28459255cca7771e35d7424230d
Victor Pereira <victorruan135@gmail.com>
2024-11-28 15:24:31 -0300

    feat: integrate GetAllChecks use case into HistoryScreenProvider

--- lib/src/presenter/screens/history/provider/history_screen_provider.dart
+++ lib/src/presenter/screens/history/provider/history_screen_provider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../domain/check/repositories/check_repository.dart';
+import '../../../../domain/check/usecases/get_all_checks.dart';
 import '../../../../external/services/generate_check_service_impl.dart';
 import '../../../../external/services/share_plus_service_impl.dart';
-import '../../../../infra/services/database/check_database_service.dart';
 import '../../../../infra/services/generate_check_service.dart';
 import '../../../../infra/services/share_check_service.dart';
 import '../controller/history_screen_controller.dart';
@@ -21,9 +22,14 @@ class HistoryScreenProvider extends StatelessWidget {
           Provider<ShareCheckService>(
             create: (_) => SharePlusCheckServiceImpl(),
           ),
+          Provider<GetAllChecks>(
+            create: (context) => GetAllChecksImpl(
+              repository: context.read<CheckRepository>(),
+            ),
+          ),
           ChangeNotifierProvider(
             create: (context) => HistoryScreenController(
-              checkService: context.read<CheckDatabaseService>(),
+              getAllChecks: context.read<GetAllChecks>(),
             ),
           ),
         ],
