ce01bf5f45af05b92b8b296e6da15f54fc5b4c4d
Victor Pereira <victorruan135@gmail.com>
2024-11-30 15:40:35 -0300

    refactor: add DeleteCheck use case to HistoryScreenProvider

--- lib/src/presenter/screens/history/provider/history_screen_provider.dart
+++ lib/src/presenter/screens/history/provider/history_screen_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../../domain/check/repositories/check_repository.dart';
+import '../../../../domain/check/usecases/delete_check.dart';
 import '../../../../domain/check/usecases/get_all_checks.dart';
 import '../../../../domain/check/usecases/share_check.dart';
 import '../controller/history_screen_controller.dart';
@@ -22,6 +23,7 @@ class HistoryScreenProvider extends StatelessWidget {
             create: (context) => HistoryScreenController(
               getAllChecks: context.read<GetAllChecks>(),
               shareCheck: context.read<ShareCheck>(),
+              deleteCheck: context.read<DeleteCheck>(),
             ),
           ),
         ],
