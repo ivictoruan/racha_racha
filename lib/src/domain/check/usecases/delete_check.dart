3764da18ad1b6ea7aebb76e409e2ae9554ce1b53
Victor Pereira <victorruan135@gmail.com>
2024-11-30 15:39:35 -0300

    refactor: update DeleteCheck use case to accept Check parameter and implement deletion logic

--- lib/src/domain/check/usecases/delete_check.dart
+++ lib/src/domain/check/usecases/delete_check.dart
@@ -1,15 +1,19 @@
 import 'package:dartz/dartz.dart';
+import '../repositories/check_repository.dart';
 
+import '../entities/check.dart';
 import '../errors/erros.dart';
 
 abstract class DeleteCheck {
-  Future<Either<Failure, bool>> call();
+  Future<Either<Failure, void>> call({required Check check});
 }
 
 class DeleteCheckImpl implements DeleteCheck {
+  final CheckRepository repository;
+
+  DeleteCheckImpl({required this.repository});
   @override
-  Future<Either<Failure, bool>> call() {
-    // TODO: implement call
-    throw UnimplementedError();
+  Future<Either<Failure, void>> call({required Check check}) {
+    return repository.deleteCheck(check: check);
   }
 }
